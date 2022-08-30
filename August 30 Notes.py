ec8de5ce859d9e0a66950dd317e53e9c277b6ee6
Oscar Ramirez <47543009+Oscar-Ramirez@users.noreply.github.com>
2022-08-30 14:41:36 -0500

    Update August 30 Notes.py

--- August 30 Notes.py
+++ August 30 Notes.py
@@ -1,1 +1,42 @@
+print("""August 30th, 2022 Computer Science Notes
 
+\033[1m What is a System? \033[0m
+A system is not just hardware + sofeware, it also includes people and immediate environment. A system may include: training employees and/or users, as well as maintaining and protecting data or hardware.
+
+A new system should improve the cost-to-benefit ratio in contrast to not implementing the system.
+
+There are a bunch of reason to implement a new system: replace a current system because it's inefficient, no longer suitable for desired tasks, or redunant or out-dated.
+The system may be added for a purpose that is new for an organization.
+
+\033[1m Legacy Systems \033[0m
+
+An Example would be a hospital and the systems it uses: Clinical, Accounting, Pharmacy, and Purchasing. These systems sometimes need to communicate with each other but are also sometimes stored in indivisual silos and silos do not communcate with each other. 
+\033[1m Multiple systems that do not communicate with each other are called Legacy Systems \033[0m
+
+There are some situtations where moderns systems have to work together with legacy systems.
+An example would be, if the legacy system contains a lot of data it is not realistic to transfer all the data unless there is an automated way to.
+
+\033[1m Planning \033[0m
+When planning on implementing a new system, including discussing new hardware, changing potential locations of technology, new policiess, new training and upskilling, and potentially hiring and firing employees as well.
+
+There is also a varity of issues that should be taken into consideration during the planning process:
+A lack of ...
+  - Organizational & Bussiness strategy
+  - Stakeholder &end-user participation
+  -End-user 'ownership'
+
+Attention to ...
+  -Required training
+  -Organizational issues, inclding group culture
+  -Design of tasks & job roles
+  -Overall usability of the system
+
+After considering the issues mentioned above a feasibility study should be conducted, the study should cover different areas. A common acronym is TELOS.
+
+T: Technical (Here all hardware and software would be justified in whether it is necessary for the new system)
+E: Economic (Relates to the budget for a project and how money will be spent, financial costs for purchasing new hardware or software, cost on training staff or timelines running past deadlines)
+L: Legal (are there any conflicts between the proposed system and any local laws, regulation, etc.)
+O:
+S:
+
+""")
